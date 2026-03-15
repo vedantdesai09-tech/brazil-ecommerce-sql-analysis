3ed9365466d68d6adfecdfb13c81af909abc4431
vedant desai <vedant.desai09@gmail.com>
2026-03-15 17:11:08 +0530

    Update 03_seller_analysis.sql

--- analysis/03_seller_analysis.sql
+++ analysis/03_seller_analysis.sql
@@ -1,1 +1,14 @@
-
+--1️⃣ which seller generate the most revenue also show there orders and aov 
+select 
+	s.seller_id ,
+	sum(oi.price) as total_revenue , 
+	count(distinct(o.order_id)) as total_orders ,
+	round(sum(oi.price)/count(distinct(o.order_id)),2) as aov  
+from order_items as oi 
+right join sellers as s
+	on s.seller_id = oi.seller_id
+join orders as o 
+	on o.order_id = oi.order_id 
+group by s.seller_id 
+order by total_revenue desc 
+limit 10 ; 
